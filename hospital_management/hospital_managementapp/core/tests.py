4bf86cd37c2f4199dff45f1408b9db3dfee13c4f
Josephdada01 <ridwandada500@gmail.com>
2024-03-22 17:21:18 +0100

    made changes to the test

--- hospital_management/hospital_managementapp/core/tests.py
+++ hospital_management/hospital_managementapp/core/tests.py
@@ -5,11 +5,11 @@ from rest_framework import status
 from django.urls import reverse
 from core.models import Doctor, Patient, Appointment
 
-# Create your tests here.
 class DoctorPatientAppointmentTests(TestCase):
-    """a class for the testing"""
+    """A class for testing"""
+
     def setUp(self):
-        """setting up the test"""
+        """Setting up the test"""
         self.client = APIClient()
         self.user_doctor = User.objects.create_user(username='doctor', 
                                                     password='testpassword')
@@ -17,57 +17,55 @@ class DoctorPatientAppointmentTests(TestCase):
                                                      password='testpassword')
         self.client.force_authenticate(user=self.user_doctor)
 
-
     def test_doctor_signup(self):
         """
-        test for doctor signing up, this allows doctors to signup 
-        successfully
-        and sending the status 201
+        Test for doctor signing up, this allows doctors to signup 
+        successfully and sending the status 201
         """
         response = self.client.post(reverse('doctor-list-create'), 
                                     data={'name': 'Dr. Joseph', 
-                                            'email': 'doctorjoe@gmail.com', 
-                                            'phone': '2348124555883'})
+                                          'email': 'doctorjoe@gmail.com', 
+                                          'phone': '2348124555883'})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        
     def test_patients_signup(self):
         """
-        test for patients not being able to signup,
+        Test for patients not being able to signup,
         will return status 403. denies the request
-            
         """
         response = self.client.post(reverse('patient-list-create'), 
-                                    data={'name': 'Joseph Dada', 'email': 
-                                            'josephdada@gmail.com', 
-                                            'phone': '2348163860056'})
+                                    data={'name': 'Joseph Dada', 
+                                          'email': 'josephdada@gmail.com', 
+                                          'phone': '2348163860056'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
 
     def test_create_patient_login(self):
         """
-        test for doctor creating account for patient
+        Test for doctor creating account for patient
         it returns status 201 for success
         """
         patient = Patient.objects.create(user=self.user_patient, 
-                                            name='John Doe', 
-                                            email='johndoe@example.com', 
-                                            phone= '09199901111')
+                                         name='John Doe', 
+                                         email='johndoe@example.com', 
+                                         phone='09199901111')
         response = self.client.post(reverse('patient-list-create'), 
-                                    data={'name': 'Jane Doe', 'email': 
-                                            'janedoe@example.com', 
-                                            'phone': '2348114567890'})
+                                    data={'name': 'Jane Doe', 
+                                          'email': 'janedoe@example.com', 
+                                          'phone': '2348114567890'})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
 
     def test_patient_login_and_book_appointment(self):
         """Test for patient login and book appointment, returning status success"""
         self.client.force_authenticate(user=self.user_patient)
+        doctor = Doctor.objects.create(user=self.user_doctor, 
+                                       name='Dr. Smith', 
+                                       email='drsmith@example.com', 
+                                       phone='1234567890')
         response = self.client.post(reverse('appointment-list-create'), 
-                                    data={'doctor': 1, 'patient': 1, 
-                                            'appointment_time': '2024-03-25T09:00:00'})
+                                    data={'doctor': doctor.id, 
+                                          'patient': 1, 
+                                          'appointment_time': '2024-03-25T09:00:00'})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
 
     def test_patient_can_book_appointment_with_any_doctor(self):
         """
@@ -75,16 +73,16 @@ class DoctorPatientAppointmentTests(TestCase):
         doctor.
         """
         self.client.force_authenticate(user=self.user_patient)
-        doctor = Doctor.objects.create(user=self.user_doctor, name='Dr. Joseph', 
-                                            email='josephdada@example.com', 
-                                            phone= '234814567890')
+        doctor = Doctor.objects.create(user=self.user_doctor, 
+                                       name='Dr. Smith', 
+                                       email='drsmith@example.com', 
+                                       phone='1234567890')
         response = self.client.post(reverse('appointment-list-create'), 
-                                    data={'doctor': doctor.id, 'patient': 1, 
-                                            'appointment_time': '2024-03-25T10:00:00'})
+                                    data={'doctor': doctor.id, 
+                                          'patient': 1, 
+                                          'appointment_time': '2024-03-25T10:00:00'})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        
-    
     def test_only_booked_doctor_can_view_patient_information(self):
         """
         This test ensures that the system correctly 
@@ -93,12 +91,17 @@ class DoctorPatientAppointmentTests(TestCase):
         appointment with the patient to view that patient's information.
         """
         self.client.force_authenticate(user=self.user_patient)
-        patient = Patient.objects.create(user=self.user_patient, name='John Doe', 
-                                            email='johndoe@example.com', 
-                                            phone= '2348136543210')
-        appointment = Appointment.objects.create(doctor=Doctor.objects.first(), 
-                                                    patient=patient, 
-                                                    appointment_time='2024-03-25T09:00:00')
+        doctor = Doctor.objects.create(user=self.user_doctor, 
+                                       name='Dr. Smith', 
+                                       email='drsmith@example.com', 
+                                       phone='1234567890')
+        patient = Patient.objects.create(user=self.user_patient, 
+                                         name='John Doe', 
+                                         email='johndoe@example.com', 
+                                         phone='2348136543210')
+        appointment = Appointment.objects.create(doctor=doctor, 
+                                                 patient=patient, 
+                                                 appointment_time='2024-03-25T09:00:00')
         self.client.force_authenticate(user=self.user_doctor)
         response = self.client.get(reverse('appointment-list-create'))  # Assuming appointment-list-create lists all appointments
         self.assertEqual(response.status_code, status.HTTP_200_OK)  # Assuming it returns HTTP 200 for authenticated users
