11b75ca94b6e858f31d05a8f8e5b27b7636b0f6b
Josephdada01 <ridwandada500@gmail.com>
2024-03-22 18:29:09 +0100

    initil commit

--- hospital_management/hospital_managementapp/core/forms.py
+++ hospital_management/hospital_managementapp/core/forms.py
@@ -4,33 +4,13 @@ from django import forms
 from .models import User
 
 class DoctorSignUpForm(UserCreationForm):
-    name = forms.CharField(max_length=30)
-    email = forms.EmailField()
-    phone = forms.IntegerField()
-
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('username', 'name', 'email', 'phone', 'password1', 'password2')
+        fields = ('username', 'password1', 'password2')
 
     def save(self, commit=True):
         user = super().save(commit=False)
         user.is_doctor = True
-        if commit:
-            user.save()
-        return user
-
-class PatientSignUpForm(UserCreationForm):
-    name = forms.CharField(max_length=30)
-    email = forms.EmailField()
-    phone = forms.IntegerField()
-
-    class Meta(UserCreationForm.Meta):
-        model = User
-        fields = ('username', 'name', 'email', 'phone', 'password1', 'password2')
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.is_doctor = False
         if commit:
             user.save()
         return user
