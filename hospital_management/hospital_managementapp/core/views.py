a294520f34c44bf0d6d5c8157c711b0f5054259d
Josephdada01 <ridwandada500@gmail.com>
2024-03-22 18:41:00 +0100

    remove unneccsary files

--- hospital_management/hospital_managementapp/core/views.py
+++ hospital_management/hospital_managementapp/core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from .forms import DoctorSignUpForm, PatientSignUpForm
+from .forms import DoctorSignUpForm
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from core.serializers import DoctorSerializer, PatientSerializer, AppointmentSerializer
@@ -21,17 +21,6 @@ class DoctorSignUpAPIView(APIView):
             return Response(status=status.HTTP_201_CREATED)
         return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class PatientSignUpAPIView(APIView):
-    """
-    API endpoint for patient signup
-    """
-    def post(self, request):
-        form = PatientSignUpForm(request.data)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return Response(status=status.HTTP_201_CREATED)
-        return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class DoctorAPIView(generics.ListCreateAPIView):
     """
