71daf855a8fdd5bed47d0d71f2a6231df2f9f4ac
Josephdada01 <ridwandada500@gmail.com>
2024-04-13 11:53:10 +0100

    Removed the old files

--- hospital_management/hospital_managementapp/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from core.models import Doctor, Patient, Appointment
-# Register your models  here.
-admin.site.register(Doctor)
-admin.site.register(Patient)
-admin.site.register(Appointment)
-
